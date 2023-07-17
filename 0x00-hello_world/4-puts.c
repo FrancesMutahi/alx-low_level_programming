2c7ebde0ec8f5eac844d047a8d14073edb88c200
FrancesMutahi <francesnjambimutahi@gmail.com>
2023-07-17 11:25:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -> where execution starts. This program prints  phrase and a new line
+*Return: 0 -> returns zero
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle" \n);
+	return (0);
+}
+
