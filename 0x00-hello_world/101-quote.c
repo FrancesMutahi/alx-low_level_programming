5d5e01bf4af725d9dc1a390c4a249bf52e6d49e9
FrancesMutahi <francesnjambimutahi@gmail.com>
2023-07-17 12:29:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main -> This file prints a standard error
- * Return 1 -> the program should return 1
+ * Return: 1 -> the program should return 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
