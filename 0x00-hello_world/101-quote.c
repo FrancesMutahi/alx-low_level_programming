156ee8c8d634d5446c0c52700b3d4b0b2b044ccb
FrancesMutahi <francesnjambimutahi@gmail.com>
2023-07-17 12:26:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -> This file prints a phrase without printf or puts
- * Return: 1 -> the program should return 1
+ * main -> This file prints a standard error
+ * Return 1 -> the program should return 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
